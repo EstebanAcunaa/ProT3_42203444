5948ec1ad40170e03145618ae6abd29c6031d2ee
Estebanacuna <estebanac43@gmail.com>
2025-06-28 11:51:57 -0300

    Configurar navbar con logins

--- app/Views/front/navbar_view.php
+++ app/Views/front/navbar_view.php
@@ -19,8 +19,53 @@
       <div class="btn btn-secundary active btnUser btn-sm">
       <a href="">ADMIN: <?php echo session('nombre');?></a>
       </div>
-      
+      <!-- NAVBAR PARA ADMIN que pueden comprar -->
+
+      <a class="navbar-brand" href="#" ></a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="registro">Registrarse</a>
+          </li>
+          <li>
+            <a class="nav-link" href="Login">Login</a>
+          </li> 
+          <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url('/logout'); ?>" tabindex="-1" aria-disabled="true">Cerrar Sesión</a>
+          </li>
+        </ul>
+      </div>
+
+      <?php elseif(session()->perfil_id == 2): ?>
+  <div class="btn btn-info active btnUser btn-sm">
+    <a href="#">CLIENTE: <?php echo session('nombre'); ?> </a>
   </div>
+  <!-- NAVBAR PARA CLIENTES que pueden comprar -->
+
+  <a class="navbar-brand" href="#"></a>
+  <div class="collapse navbar-collapse" id="">
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="quienes_somos">Quiénes Somos</a>
+      </li>
+      <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Categorias
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Reloj Mesa</a></li>
+            <li><a class="dropdown-item" href="#">Reloj Pared</a></li>
+            <li><hr class="dropdown-divider"></li>
+          </ul>
+        </li>
+      <li class="nav-item">
+        <a class="nav-link" href="<?php echo base_url('/logout'); ?>" tabindex="-1" aria-disabled="true">Cerrar Sesión</a>
+      </li>
+    </ul>
+  </div>
+<?php else: ?>
+
+    <a class="navbar-brand" href="#"></a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
@@ -46,11 +91,13 @@
             <li><a class="dropdown-item" href="#">Reloj Pared</a></li>
             <li><hr class="dropdown-divider"></li>
           </ul>
+        </li>
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
         <button class="btn btn-outline-success" type="submit">Buscar</button>
       </form>
+      <?php endif;?>
     </div>
   </div>
 </nav>
